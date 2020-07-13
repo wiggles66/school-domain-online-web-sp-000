7178f22b060c43a957ee63fbb27099bcc40b6727
Michael Waide <mvwaide@gmail.com>
2020-07-13 16:40:08 +0000

    Automatically backed up by Learn

--- lib/school.rb
+++ lib/school.rb
@@ -1,44 +1,44 @@
 class School
   attr_accessor :name, :roster
-  
+
   def initialize(name)
     @name = name
     @roster = {}
-  end 
-  
+  end
+
   def add_student(student, level)
     # this sort of means #=> a || a = b ... or #=> x || x = y
     # it's freak'n weird
     roster[level] ||= []
     roster[level] << student
-  end 
-  
+  end
+
   def grade(level)
-    roster.detect do |x, y| 
+    roster.detect do |x, y|
       if x == level
-        return y 
-      end 
-    end 
-  end 
-  
-# This works, but it isn't passing learn.co's tests cause of 
+        return y
+      end
+    end
+  end
+
+# This works, but it isn't passing learn.co's tests cause of
 # how it displays syntactically...but the same information is there
-#  def sort 
-#    roster.sort_by do |x, y| 
-#      x 
-#    end 
-#  end 
+#  def sort
+#    roster.sort_by do |x, y|
+#      x
+#    end
+#  end
 
 # Here's learn.co's solution:
-def sort 
+def sort
   nu_hash = {}
-  roster.each do |x, y| 
-    nu_hash[x] = y.sort 
-  end 
+  roster.each do |x, y|
+    nu_hash[x] = y.sort
+  end
   nu_hash
-end 
-  
-end 
+end
+
+end
 
 
 
